f7aa0223008f26d99cb417134851d3bdbae3b970
Federico Cavalieri <f@28.io>
2016-05-11 19:49:35 +0200

    Refactoring

---
+++ modules/swagger-codegen/src/main/java/io/cellstore/codegen/CellStoreCSharpClientCodegen.java
@@ -0,0 +1,11 @@
+package io.cellstore.codegen;
+
+import io.swagger.codegen.languages.CSharpClientCodegen;
+
+public class CellStoreCSharpClientCodegen extends CSharpClientCodegen
+{
+  @Override
+  public String getName() {
+    return "cellstore-csharp";
+  }
+}
